0b7ca9e88dff837510b62cc23d1cf323075e798b
Quân <quan22032003@gmail.com>
2023-09-17 08:20:45 +0700

    File upload

---
+++ tạo form nhập liệu với Model/WebApplication1/Models/DanhSachMayTinh.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public static class DanhSachMayTinh
+    {
+        public static List<MayTinh> listMayTinh = new List<MayTinh>();
+    }
+}
