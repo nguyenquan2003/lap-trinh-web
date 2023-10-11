d7371c9465b548874f25e4b7e0b27923e3978225
Quân <quan22032003@gmail.com>
2023-10-11 08:05:05 +0700

    File upload

---
+++ tạo form upload file/WebApplication1/Controllers/KhachHangController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        public ActionResult DanhSach()
+        {
+            return View(DanhSachKhachHang.dsKhachHang);
+        }
+        public ActionResult ThemMoi()
+        {
+            return View(new KhachHang(){ ID=0 });
+        }
+        [HttpPost]
+        public ActionResult ThemMoi(KhachHang model, HttpPostedFileBase fileAnh)
+        {
+            if(model.ID == 0)
+            {
+                ModelState.AddModelError("", "ID phải nhập > 0");
+                return View(model);
+            }
+            if (fileAnh.ContentLength > 0)
+            {
+                // Luu file
+                string rootFolder = Server.MapPath("/Data/");
+                string pathImg = rootFolder+fileAnh.FileName;
+                fileAnh.SaveAs(pathImg);
+
+                // Lưu thuộc tính url Hinh Anh
+                model.UrlHinhAnh = "/Data/" + fileAnh.FileName;
+            }
+            DanhSachKhachHang.dsKhachHang.Add(model);
+            return RedirectToAction("DanhSach");
+        }
+    }
+}
---
+++ tạo form upload file/WebApplication1/Models/KhachHang.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class KhachHang
+    {
+        //1. ID khách hàng : số 1,2,3,4
+        //2. Tên khách hàng
+        //3. Số điện thoại
+        //4. Địa chỉ nhận hàng
+        //5. Email
+        //6. Giới tính : Nam / Nữ (sử dụng select để nhập)
+        public int ID { set; get; }
+        public string TenKhachHang { set; get; }
+        public string SoDienThoai { set; get; }
+        public string DiaChiNhanHang { set; get; }
+        public string Email { set; get; }
+        public string GioiTinh { set; get; }
+        public string UrlHinhAnh { get; set; }
+    }
+}
